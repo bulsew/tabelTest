f8c94b04a14b0e7e37bae55539baf8bc9c28fc31
bulsew <981518587@qq.com>
2024-05-11 11:25:44 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <QApplication>
+
+#include<string>
+#include"table.h"
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+
+//    vector<tableRows> roows;
+//    roows.push_back(tableRows("student",type::CHAR,20));
+//    roows.push_back(tableRows("age",type::INT));
+    Table c ("student");
+//    c.instertTOTable("29|'bdn'","age|student");
+    if(c.translateFromConstrainMessageAndCheck("1\n*\nin|99|300|200|400|500",type::INT,300))
+        cout<<666<<endl;
+              else
+              cerr<<"nsa"<<endl;
+    return a.exec();
+}
